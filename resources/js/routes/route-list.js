4c1be42e1c256a8b59cad0289267972f2837ea8f
tienminhvy <tienminhvydev@gmail.com>
2023-03-16 12:51:42 +0700

    Hoàn tất các template - Bài tập - Thành viên lớp học - Popup bài tập Note: - Đã responsive

--- resources/js/routes/route-list.js
+++ resources/js/routes/route-list.js
@@ -5,5 +5,8 @@ const route = new Route();
 route.addRoute('/', './templates/dashboard.html', DashBoard.index);
 route.addRoute('404', './templates/404.html');
 route.addRoute('/info', './templates/dashboard.html', DashBoard.index);
+route.addRoute('/people', './templates/people.html', DashBoard.index);
+route.addRoute('/tasks', './templates/tasks.html', DashBoard.index);
+route.addRoute('/popup', './templates/popup.html', DashBoard.index);
 
 export var routeList = route.getUrlRoutes();
