0af4c3d2eae9d2660a90a9bac754497e4a5f1379
Nguyen Thanh Duy <thanhduy191103@gmail.com>
2023-03-17 18:56:33 +0700

    base login

---
+++ resources/js/pages/login.js
@@ -0,0 +1,32 @@
+export class Login {
+    static index() {
+        var form = document.getElementById('login-form');
+        let HEADERS = { 'Content-Type': 'application/json' };
+        var currentURL =
+            location.protocol + '//' + location.host + '/api/login';
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            // var inputs = form.querySelectorAll('input');
+            //var formData = new FormData(form);
+
+            const data = new FormData(form);
+            const formData = {};
+            for (const [key, value] of data) {
+                formData[key] = value;
+            }
+
+            axios
+                .post(currentURL, formData, {
+                    headers: {
+                        Accept: 'application/vnd.api+json',
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then((res) => {
+                    console.log(res);
+                    //  console.log(res.data);
+                });
+        });
+    }
+}
