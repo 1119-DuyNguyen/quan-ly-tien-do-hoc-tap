5a31e2dfc5abdbbf311b4bf672e8d5301abaf062
Nguyen Thanh Duy <thanhduy191103@gmail.com>
2023-04-17 23:21:45 +0700

    fix route list- change login to authendication

--- resources/js/routes/route-list.js
+++ resources/js/routes/route-list.js
@@ -2,19 +2,19 @@ import { Route } from '../abstract/classes.js';
 import { Class } from '../pages/class.js';
 
 import { DashBoard } from '../pages/dashboard.js';
-import { Info } from "../pages/info.js";
-import { Login } from '../pages/login.js';
+import { Info } from '../pages/info.js';
 
 import { Authentication } from '../pages/authentication.js';
 import { Role } from '../pages/admin/role.js';
-
 
 const route = new Route();
 
 route.addRoute('/', Authentication.login, { title: 'Login' }, 'templates/login.html');
 route.addRoute('sinh-vien/dashboard', DashBoard.index, { title: 'DashBoard' }, 'templates/dashboard.html');
 route.addRoute('404', '', { title: '404' }, 'templates/404.html');
-route.addRoute('info', Info.index, {}, 'templates/info.html');
+route.addRoute('sinh-vien/info', Info.index, {}, 'templates/info.html');
+route.addRoute('giang-vien/info', Info.index, {}, 'templates/info.html');
+
 route.addRoute('people', DashBoard.index, {}, 'templates/people.html');
 route.addRoute('popup', DashBoard.index, {}, 'templates/popup.html');
 route.addRoute('class', Class.index, { title: 'classroom' }, 'templates/class.html');
