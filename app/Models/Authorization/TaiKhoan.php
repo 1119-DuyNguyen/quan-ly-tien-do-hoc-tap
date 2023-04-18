ca4c2aa7eae9e158e603bab5499bc791c6d2335c
Dak Lod <trandacloc123@gmail.com>
2023-04-18 11:40:17 +0700

    update import giangvien + hoc phan

--- app/Models/Authorization/TaiKhoan.php
+++ app/Models/Authorization/TaiKhoan.php
@@ -19,7 +19,7 @@ class TaiKhoan extends Authenticatable
         'lop_hoc_id' => 'int',
     ];
 
-    protected $fillable = ['ten', 'ten_dang_nhap', 'mat_khau', 'khoa_id', 'lop_hoc_id'];
+    protected $fillable = ['ten', 'ten_dang_nhap', 'mat_khau', 'khoa_id', 'lop_hoc_id', 'quyen_id'];
 
     protected $hidden = ['remember_token', 'mat_khau'];
 
@@ -84,9 +84,9 @@ class TaiKhoan extends Authenticatable
     }
     /**
      *  config passport
-     * 
+     *
      * @param string $username
-     * 
+     *
      * @return TaiKhoan
      */
     public function findForPassport(string $username): TaiKhoan
