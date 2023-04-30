3d0a8204a72a060a07795cd3889e64ca720eddb3
tienminhvy <tienminhvydev@gmail.com>
2023-04-30 20:57:34 +0700

    Tạm xong phần thống kê

--- app/Http/Controllers/Admin/AnalyticsController.php
+++ app/Http/Controllers/Admin/AnalyticsController.php
@@ -14,25 +14,68 @@ class AnalyticsController extends ApiController
     public function index(Request $request)
     {
         $tong_so_sv = count(DB::table("tai_khoan")->select("*")->where('quyen_id', '=', '1')->get());
-        $so_sv_tot_nghiep = count(DB::table("tinh_trang_sinh_vien")->select("*")->where('da_tot_nghiep', '=', '1')->get());
-        $so_sv_chua_tot_nghiep = $tong_so_sv - $so_sv_tot_nghiep;
+        $so_sv_tot_nghiepTong = count(DB::table("tinh_trang_sinh_vien")->select("*")->where('da_tot_nghiep', '=', '1')->get());
+        $so_sv_chua_tot_nghiepTong = $tong_so_sv - $so_sv_tot_nghiepTong;
 
         // Xác định sinh viên trễ hạn tốt nghiệp hay không.
 
-        $dssv_chua_tot_nghiep = DB::table("tinh_trang_sinh_vien")
+        $dssv = DB::table("tinh_trang_sinh_vien")
         ->join("lop_hoc", "tinh_trang_sinh_vien.lop_hoc_id", "=", "lop_hoc.id")
         ->join("chuong_trinh_dao_tao", "lop_hoc.chuong_trinh_dao_tao_id", "=", "chuong_trinh_dao_tao.id")
         ->join("chu_ky", "chuong_trinh_dao_tao.chu_ky_id", "=", "chu_ky.id")
-        ->select("*")->where('da_tot_nghiep', '=', '0')->get();
+        ->select("*")->get();
         // dd(count($result));
 
-        $so_sv_tre_han = 0;
+        $so_sv_tre_hanTong = 0;
 
-        foreach ($dssv_chua_tot_nghiep as $sv) {
-            if ($sv->nam_ket_thuc < date("Y")) $so_sv_tre_han++;
+        foreach ($dssv as $sv) {
+            if ($sv->nam_ket_thuc < date("Y") && $sv->da_tot_nghiep == "0") 
+                $so_sv_tre_hanTong++;
         }
+
+        // Xác định đối với từng đợt
+
+        $ds_moc_thoi_gian = DB::table("moc_thoi_gian_tot_nghiep")->get();
+        $ds_moc_thoi_gianObj = array();
         
-        return  $this->success($so_sv_tre_han, 200, "success");
+        foreach ($ds_moc_thoi_gian as $moc_thoi_gian) {
+            $sv_tre_han = 0;
+            $sv_tong = 0;
+            $sv_da_tot_nghiep = 0;
+
+            foreach ($dssv as $sv) {
+                $sv_tong++;
+                if ($sv->nam_ket_thuc < $moc_thoi_gian->nam && $sv->da_tot_nghiep == "0") {
+                    $sv_tre_han++;
+                    continue;
+                }
+                if ($sv->da_tot_nghiep == "1") {
+                    if ($sv->moc_thoi_gian_id == $moc_thoi_gian->id) {
+                        $sv_da_tot_nghiep++;
+                        continue;
+                    }
+                }
+            }
+
+
+            $ds_moc_thoi_gianObj[$moc_thoi_gian->id] = array(
+                "tong_sv" => $sv_tong,
+                "sv_tot_nghiep" => $sv_da_tot_nghiep,
+                "sv_chua_tot_nghiep" => $sv_tong - $sv_da_tot_nghiep,
+                "sv_tre_han" => $sv_tre_han,
+            );
+        }
+
+        $object = (object) array(
+            "tong_sv" => $tong_so_sv,
+            "sv_tot_nghiep" => $so_sv_tot_nghiepTong,
+            "sv_chua_tot_nghiep" => $so_sv_chua_tot_nghiepTong,
+            "sv_tre_han" => $so_sv_tre_hanTong,
+            "moc_thoi_gian_tot_nghiep" => $ds_moc_thoi_gian,
+            "dot" => (object) $ds_moc_thoi_gianObj
+        );
+        
+        return  $this->success($object, 200, "success");
     }
 
     /**
--- resources/js/pages/admin/analytics.js
+++ resources/js/pages/admin/analytics.js
@@ -1,10 +1,42 @@
 export class Analytics {
     static URL_REQ = location.protocol + '//' + location.host + '/api/admin/analytics';
     static async index() {
+
+        function renderData(elem, tong_sv, sv_tot_nghiep, sv_tre_han, sv_chua_tot_nghiep) {
+            elem[0].innerHTML = tong_sv;
+            elem[1].innerHTML = parseFloat((sv_tot_nghiep / tong_sv) * 100) + "%";
+            elem[2].innerHTML = parseFloat((sv_tre_han / sv_chua_tot_nghiep) * 100) + "%";
+            elem[3].innerHTML = parseFloat((sv_chua_tot_nghiep / tong_sv) * 100) + "%";
+        }
+
         axios.get(Analytics.URL_REQ).then(function (response) {
             let data = response.data.data;
 
             console.log(data);
+            
+            let analytics__item = document.querySelectorAll(".analytics__item__title");
+
+            renderData(analytics__item, data.tong_sv, data.sv_tot_nghiep, data.sv_tre_han, data.sv_chua_tot_nghiep);
+
+            let analytics__selector = document.querySelector("#analytics__selector");
+            
+            data.moc_thoi_gian_tot_nghiep.forEach(moc_tg => {
+                let elem;
+                elem = document.createElement('option');
+                elem.innerText = moc_tg.ten;
+                elem.value = moc_tg.id;
+                analytics__selector.add(elem);
+            });
+
+            analytics__selector.onchange = () => {
+                if (analytics__selector.value != -1) {
+
+                    let dataToGet = data.dot[analytics__selector.value];
+
+                    renderData(analytics__item, dataToGet.tong_sv, dataToGet.sv_tot_nghiep, dataToGet.sv_tre_han, dataToGet.sv_chua_tot_nghiep);
+
+                } else renderData(analytics__item, data.tong_sv, data.sv_tot_nghiep, data.sv_tre_han, data.sv_chua_tot_nghiep);
+            }
         })
     }
 }
